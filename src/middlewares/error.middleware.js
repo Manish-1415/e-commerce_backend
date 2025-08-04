1f8b0ba005e872ae12941caf5658d5dfa493424f
Manish-1415 <manishshinde1415@gmail.com>
2025-08-04 09:38:26 +0530

    Error middleware added .

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,13 @@
-const errorMiddleware = (err , req , res , next) => {
+const errorMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something Went Wrong";
 
-}
-
+  return res
+  .status(statusCode)
+  .json({
+    statusCode,
+    message,
+  });
+};
 
 export default errorMiddleware;
